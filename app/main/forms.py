e34c868e58cb93025f78b4056a24db25e83e6bb1
peter kuria <you@example.com>
2020-01-22 01:54:29 -0500

    added formfields to recommedation nform

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,10 +1,11 @@
 from ..models import Recommedation
 from flask_wtf import FlaskForm
-from wtforms import StringField,TextAreaField,SubmitField
+from wtforms import StringField,TextAreaField,SubmitField,DateField
 
 
 class RecommedationForm(FlaskForm):
 
     heading= StringField('title',validators=[Required()])
     content = TextAreaField('content body', validators=[Required()])
+    time= start = DateField('Date', format='%m/%d/%Y')
     submit = SubmitField('Submit')
