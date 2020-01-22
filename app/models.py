56d481dabeb96b7bbab9242bbac449f7443742dd
peter kuria <you@example.com>
2020-01-22 01:33:47 -0500

    added model fields to recommedation class

--- app/models.py
+++ app/models.py
@@ -4,9 +4,15 @@
 
 
 
+class Comic(db.Model):
+    __tablename__='comic'
+    id=db.Column(db.Integer,primary_key=True)
 
 
 
 class Recommedation(db.Model):
     __tablename__='recommedations'
-    
+    id=db.Column(db.Integer,primary_key=True)
+    heading=db.Column(db.String(255),index=True)
+    content=db.Column(db.Text)
+    comic_id=db.Column(db.Integer,db.ForeignKey('comic.id'))
