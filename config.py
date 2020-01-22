9b95609848594b1979c50e9d689172feed8d9536
John <johnopana2016@gmail.com>
2020-01-22 10:02:35 +0300

    added class config

--- config.py
+++ config.py
@@ -1,1 +1,14 @@
 import os
+class Config:
+    """Main configurations class"""
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    #SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:opanaopana@localhost/'
+    SECRET_KEY = "ghihdslkj;sa;lksd;lk;salkj" #os.environ.get("SECRET_KEY")
+    UPLOADED_PHOTOS_DEST = 'app/static/photos'
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
