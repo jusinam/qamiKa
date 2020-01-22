4af7839ac0d21736f87dc757b9cbed0ac18bbd54
peter kuria <you@example.com>
2020-01-22 01:49:43 -0500

    added a recommedtation form class

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,3 +1,10 @@
 from ..models import Recommedation
 from flask_wtf import FlaskForm
 from wtforms import StringField,TextAreaField,SubmitField
+
+
+class RecommedationForm(FlaskForm):
+
+    heading= StringField('title',validators=[Required()])
+    content = TextAreaField('content body', validators=[Required()])
+    submit = SubmitField('Submit')
